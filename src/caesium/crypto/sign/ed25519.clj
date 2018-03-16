4d7b5fc0ee972b710c483842e730a5e80ed97e43
David Walker <david@davidwalker.org>
2018-03-16 12:20:55 +0000

    Use - instead of _ where possible

--- src/caesium/crypto/sign/ed25519.clj
+++ src/caesium/crypto/sign/ed25519.clj
@@ -13,17 +13,16 @@
   [curve25519_pk ed25519_pk]
   (b/call! crypto_sign_ed25519_pk_to_curve25519 curve25519_pk ed25519_pk))
 
-(defn ed25519_pk_to_curve25519 [ed25519_pk]
-  (let [curve25519_pk (bb/alloc publickeybytes)]
-    (ed25519-pk-to-curve25519-buf! curve25519_pk (bb/->indirect-byte-buf ed25519_pk))
-    (bb/->bytes curve25519_pk)))
-
+(defn ed25519-pk-to-curve25519 [ed25519-pk]
+  (let [curve25519-pk (bb/alloc publickeybytes)]
+    (ed25519-pk-to-curve25519-buf! curve25519-pk (bb/->indirect-byte-buf ed25519-pk))
+    (bb/->bytes curve25519-pk)))
 
 (defn ed25519-sk-to-curve25519-buf!
   [curve25519_sk ed25519_sk]
   (b/call! crypto_sign_ed25519_sk_to_curve25519 curve25519_sk ed25519_sk))
 
-(defn ed25519_sk_to_curve25519 [ed25519_sk]
-  (let [curve25519_sk (bb/alloc publickeybytes)]
-    (ed25519-sk-to-curve25519-buf! curve25519_sk (bb/->indirect-byte-buf ed25519_sk))
-    (bb/->bytes curve25519_sk)))
+(defn ed25519-sk-to-curve25519 [ed25519-sk]
+  (let [curve25519-sk (bb/alloc publickeybytes)]
+    (ed25519-sk-to-curve25519-buf! curve25519-sk (bb/->indirect-byte-buf ed25519-sk))
+    (bb/->bytes curve25519-sk)))
